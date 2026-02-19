b9c569ecf6fded15725741332056c3d2c65ca166
vtu27631-byte <vtu27631@veltech.edu.in>
2026-02-19 14:45:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from fastapi.responses import JSONResponse
 from datetime import datetime
 
 app = FastAPI(title="PharmaGuard API")
+ from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # ------------------------
 # VCF PARSER
